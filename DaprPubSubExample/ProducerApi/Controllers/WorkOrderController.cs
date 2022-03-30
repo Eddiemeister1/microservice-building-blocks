d14a3466bf23707e06964c3dcb864efa187e2d5c
Eddiemeister1 <eduardodotel2@gmail.com>
2022-03-30 09:14:47 -0400

    DaprPubSubExample is implemented

--- DaprPubSubExample/ProducerApi/Controllers/WorkOrderController.cs
+++ DaprPubSubExample/ProducerApi/Controllers/WorkOrderController.cs
@@ -1,24 +1,29 @@
 ﻿using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ProducerApi.Controllers
-{
-    public class WorkOrderController : ControllerBase
-    {
-        private readonly DaprClient _daprClient;
+namespace ProducerApi.Controllers;
 
-        [HttpPost("work-orders")]
-        public async Task<ActionResult> Create([FromBody] WorkOrder workOrder)
-        {
-            //write this to the message queue for someone else to worry about.
-            await _daprClient.PublishEventAsync("work-items-pub-sub", "work-items", workOrder);
-            var response = new WorkOrderResponse(workOrder, WorkOrderStatus.Pending);
-            return Ok(workOrder);
-        }
+public class WorkOrderController : ControllerBase
+{
+    private readonly DaprClient _daprClient;
+
+    public WorkOrderController(DaprClient daprClient)
+    {
+        _daprClient = daprClient;
     }
 
-    public record WorkOrder(string job, List<string> steps);
-
-    public enum WorkOrderStatus {  Pending, Approved, Denied }
-    public record WorkOrderResponse(WorkOrder order, WorkOrderStatus status);
+    [HttpPost("work-orders")]
+    public async Task<ActionResult> Create([FromBody] WorkOrder workOrder)
+    {
+        // write this to the message queue for someoone else to worry about.
+        await _daprClient.PublishEventAsync("work-items-pub-sub", "work-items", workOrder);
+        var response = new WorkOrderResponse(workOrder, WorkOrderStatus.Pending);
+        return Ok(response);
+    }
 }
+
+
+public record WorkOrder(string job, List<string> steps);
+
+public enum WorkOrderStatus {  Pending, Approved, Denied }
+public record WorkOrderResponse(WorkOrder order, WorkOrderStatus status);
